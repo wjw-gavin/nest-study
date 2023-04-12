5a23af1a1915174fa145d42d724938d65e00f200
Gavin <gavinwjw@163.com>
2023-04-12 15:21:53 +0800

    perf: response id instead of _id

--- src/cats/dto/cat.dto.ts
+++ src/cats/dto/cat.dto.ts
@@ -1,15 +1,26 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument } from 'mongoose'
+
+export type CatDocument = HydratedDocument<Cat>
 
 @Schema()
 export class Cat {
   @Prop({ required: true })
   name: string
 
-  @Prop()
+  @Prop({ required: true })
   age: number
 
-  @Prop()
+  @Prop({ required: true })
   sex: string
 }
 
 export const CatSchema = SchemaFactory.createForClass(Cat)
+
+CatSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: (doc, ret: CatDocument) => {
+    delete ret._id
+  }
+})
