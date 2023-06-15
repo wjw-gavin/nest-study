73b3813c75986e6500c8d2b07cc8ac86eabccbcd
Gavin <gavinwjw@163.com>
2023-06-15 18:54:56 +0800

    chore: format code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { DBModule } from './db/db.module'
+import { MenuModule } from './modules/menu/menu.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { UserModule } from './modules/user/user.module'
-import { ArticleModule } from './modules/article/article.module'
 import { RoleModule } from './modules/role/role.module'
-import { MenuModule } from './modules/menu/menu.module'
+import { ArticleModule } from './modules/article/article.module'
 
 @Module({
   imports: [
