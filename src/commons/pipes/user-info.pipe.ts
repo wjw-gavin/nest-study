4dcd899ca4e3bf6d62dc9182bf16d5f8e7fa1570
Gavin <gavinwjw@163.com>
2023-07-17 17:44:40 +0800

    chore: optimize code

--- src/commons/pipes/user-info.pipe.ts
+++ src/commons/pipes/user-info.pipe.ts
@@ -5,7 +5,6 @@ import { UserService } from 'src/modules/user/user.service'
 export class UserInfoByIdPipe implements PipeTransform<any> {
   constructor(private readonly userService: UserService) {}
   async transform(value: string) {
-    console.log(value)
     const user = await this.userService.findOne(+value)
     if (user) {
       return user
