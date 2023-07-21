c8e8487cf477529b616cbfcd108081a990cd0956
Gavin <gavinwjw@163.com>
2023-07-21 13:54:24 +0800

    fix: add article service

--- src/shared/services.ts
+++ src/shared/services.ts
@@ -1,7 +1,9 @@
 import { UserService } from 'src/modules/user/user.service'
 import { RoleService } from 'src/modules/role/role.service'
+import { ArticleService } from 'src/modules/article/article.service'
 
 export default {
   UserService,
-  RoleService
+  RoleService,
+  ArticleService
 }
