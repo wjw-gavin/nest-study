2aa506248cb82851accc5726d13bbdbb39416c81
Gavin <gavinwjw@163.com>
2023-07-21 13:51:55 +0800

    chore: optimize code

--- src/commons/utils/transformStringToClassName.ts
+++ src/commons/utils/transformStringToClassName.ts
@@ -1,4 +1,5 @@
 export function transformStringToClassName(str: string): string {
+  if (!str) return
   const words = str.split('_')
   const capitalizedWords = words.map(
     (word) => word.charAt(0).toUpperCase() + word.slice(1)
