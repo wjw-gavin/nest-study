3d1c12b1f9d93ec7e2e3e753bb161b99098d9dba
Gavin <gavinwjw@163.com>
2023-08-04 18:09:17 +0800

    feat: support static files for accessing services

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import { AppModule } from './app.module'
 import { URL_PREFIX } from './commons/constants'
 import { HttpExceptionFilter } from './commons/filters/http.exception.filter'
 import { HttpSuccessInterceptor } from './commons/interceptors/http.success.interceptor'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { join } from 'node:path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
   app.use(cookieParser())
 
@@ -16,6 +18,10 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalInterceptors(new HttpSuccessInterceptor())
+
+  app.useStaticAssets(join(__dirname, '../resources/static'), {
+    prefix: '/static' // eg: http://localhost:3000/static/...png
+  })
 
   await app.listen(3000)
 
